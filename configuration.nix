485bb4864de41da98fc2597bad48fc01df2c6385
Garrett59100 <garrett59100@gmail.com>
2024-02-14 18:49:41 -0500

    Added Home-manager

--- configuration.nix
+++ configuration.nix
@@ -2,12 +2,13 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ pkgs, inputs, ... }:
 
 {
 	imports =
 		[ # Include the results of the hardware scan.
 			./hardware-configuration.nix
+			inputs.home-manager.nixosModules.default
 			#<nixos-hardware/microsoft/surface/surface-laptop-amd>
 			#<nixpkgs/nixos/modules/virtualization/qemu-vm.nix>
 		];
@@ -92,47 +93,120 @@
 		#media-session.enable = true;
 	};
 
-	# Enable touchpad support (enabled default in most desktopManager).
-	services.xserver.libinput.enable = true;
-
-	# Define a user account. Don't forget to set a password with ‘passwd’.
 	users.users.garrett = {
 		isNormalUser = true;
 		description = "Garrett Tupper";
 		extraGroups = [ "networkmanager" "wheel" "docker" ];
 		shell = pkgs.zsh;
-		packages = with pkgs; [
-			zsh
-			oh-my-zsh
-			alacritty
-			vscode-fhs
-			python3
-			syncthing
-			discord
-			appflowy
-			hyprland
-			waybar
-			mattermost-desktop
-			rofi-wayland
-			keepassxc
-			neofetch
-			docker
-			docker-client
-			kitty
-			git
-			slack
-			dbeaver
-			nodejs_21
-			openssl
-			gccgo
-			binutils
-			gnumake
-			zulu
-			zulu8
-			maven
-			postman
-			unzip
-		];
+	};
+
+	# Enable touchpad support (enabled default in most desktopManager).
+	services.xserver.libinput.enable = true;
+
+	home-manager = {
+		extraSpecialArgs = { inherit inputs; };
+		users = {
+			"garrett" = {
+        # Home Manager needs a bit of information about you and the paths it should
+        # manage.
+        home.username = "garrett";
+        home.homeDirectory = "/home/garrett";
+
+        # This value determines the Home Manager release that your configuration is
+        # compatible with. This helps avoid breakage when a new Home Manager release
+        # introduces backwards incompatible changes.
+        #
+        # You should not change this value, even if you update Home Manager. If you do
+        # want to update the value, then make sure to first check the Home Manager
+        # release notes.
+        home.stateVersion = "23.11"; # Please read the comment before changing.
+
+        # The home.packages option allows you to install Nix packages into your
+        # environment.
+        home.packages = with pkgs; [
+          # # Adds the 'hello' command to your environment. It prints a friendly
+          # # "Hello, world!" when run.
+          # pkgs.hello
+
+          # # It is sometimes useful to fine-tune packages, for example, by applying
+          # # overrides. You can do that directly here, just don't forget the
+          # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+          # # fonts?
+          # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+          # # You can also create simple shell scripts directly inside your
+          # # configuration. For example, this adds a command 'my-hello' to your
+          # # environment:
+          # (pkgs.writeShellScriptBin "my-hello" ''
+          #   echo "Hello, ${config.home.username}!"
+          # '')
+          zsh
+          oh-my-zsh
+          kitty
+          vscode-fhs
+          python3
+          syncthing
+          discord
+          appflowy
+          hyprland
+          hyprpaper
+          waybar
+          mattermost-desktop
+          rofi-wayland
+          keepassxc
+          neofetch
+          docker
+          git
+          slack
+          dbeaver
+          nodejs_21
+          openssl
+          gccgo
+          gnumake
+          zulu
+          maven
+          postman
+          unzip
+        ];
+
+        # Home Manager is pretty good at managing dotfiles. The primary way to manage
+        # plain files is through 'home.file'.
+        home.file = {
+          # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+          # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+          # # symlink to the Nix store copy.
+          # ".screenrc".source = dotfiles/screenrc;
+
+          # # You can also set the file content immediately.
+          # ".gradle/gradle.properties".text = ''
+          #   org.gradle.console=verbose
+          #   org.gradle.daemon.idletimeout=3600000
+          # '';
+        };
+
+        # Home Manager can also manage your environment variables through
+        # 'home.sessionVariables'. If you don't want to manage your shell through Home
+        # Manager then you have to manually source 'hm-session-vars.sh' located at
+        # either
+        #
+        #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+        #
+        # or
+        #
+        #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+        #
+        # or
+        #
+        #  /etc/profiles/per-user/garrett/etc/profile.d/hm-session-vars.sh
+        #
+        home.sessionVariables = {
+          # EDITOR = "emacs";
+        };
+
+        # Let Home Manager install and manage itself.
+        programs.home-manager.enable = true;
+      };
+		};
 	};
 
 	fonts = {
@@ -151,13 +225,6 @@
 
 	# Allow unfree packages
 	nixpkgs.config.allowUnfree = true;
-
-	# List packages installed in system profile. To search, run:
-	# $ nix search wget
-	environment.systemPackages = with pkgs; [
-	#  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-	#  wget
-	];
 
 	# Some programs need SUID wrappers, can be configured further or are
 	# started in user sessions.
@@ -196,19 +263,19 @@
 	# Or disable the firewall altogether.
 	# networking.firewall.enable = false;
 
-  nixpkgs.overlays = [
+	nixpkgs.overlays = [
 	(final: prev: {
-	  postman = prev.postman.overrideAttrs(old: rec {
+		postman = prev.postman.overrideAttrs(old: rec {
 		version = "20230716100528";
 		src = final.fetchurl {
-		  url = "https://web.archive.org/web/${version}/https://dl.pstmn.io/download/latest/linux_64";
-		  sha256 = "sha256-svk60K4pZh0qRdx9+5OUTu0xgGXMhqvQTGTcmqBOMq8=";
+			url = "https://web.archive.org/web/${version}/https://dl.pstmn.io/download/latest/linux_64";
+			sha256 = "sha256-svk60K4pZh0qRdx9+5OUTu0xgGXMhqvQTGTcmqBOMq8=";
 
-		  name = "${old.pname}-${version}.tar.gz";
+			name = "${old.pname}-${version}.tar.gz";
 		};
-	  });
+		});
 	})
-  ];
+	];
 
 
 
